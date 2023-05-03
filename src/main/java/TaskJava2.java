a98bcd78b06ba2b8d0657d5ae19329149f1648d9
E-i-n-z-e-l <120109050+E-i-n-z-e-l@users.noreply.github.com>
2023-05-03 15:56:10 +0800

    Update TaskJava2.java
    Пусть дан список сотрудников. 
    А) Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений.
    Б) Отсортировать по убыванию популярности.

--- src/main/java/TaskJava2.java
+++ src/main/java/TaskJava2.java
@@ -52,13 +52,13 @@ public class TaskJava2 {
         arrayList.put("Ежов", "Иван");
 
         for (String key : arrayList.keySet()) {
-            List<String> values = Collections.singletonList(arrayList.get(key));
-            System.out.println("Фамилия: " + key + "\t\t\t\t" + "Имя: " + values);
+            List<String> values = Collections.singletonList(arrayList.get(key)); 
+            System.out.println("Фамилия: " + key + "\t\t\t\t" + "Имя: " + values); // выводим в терминал HashMap для проверки введенных выше ключей и значений;
         }
 
-        HashMap<String, Integer> countMap = new HashMap<>();
+        HashMap<String, Integer> countMap = new HashMap<>(); // Создаем новый HashMap чтобы поместить в него все значения из предыдущего HashMap чтобы узнать сколько раз кажждое значение повторяется;
 
-        for (String value : arrayList.values()) {
+        for (String value : arrayList.values()) { // заносим в новый HashMap кол-во повторений значений из предыдущего HashMap
             if (countMap.containsKey(value)) {
                 countMap.put(value, countMap.get(value) + 1);
             }
@@ -66,13 +66,17 @@ public class TaskJava2 {
                 countMap.put(value, 1);
             }
         }
-        countMap.entrySet().removeIf(entry -> entry.getValue() == 1); // удаляем все ключ-значение значение которых равно 1;
-        System.out.println("Количество повторяющихся значений: " + countMap);
+        
+        countMap.entrySet().removeIf(entry -> entry.getValue() == 1); //  Метод entrySet() возвращает набор элементов коллекции countMap в виде множества объектов типа Map.Entry<K, V>, где K - тип ключа, а V - тип значения. 
+                                                                      //  метод removeIf() применяется к этому множеству, и для каждого элемента выполняется проверка на условие - если значение равно 1, то элемент удаляется из коллекции
+                                                                      //  Лямбда-выражение entry -> entry.getValue() == 1 является предикатом, который возвращает true для элементов, у которых значение равно 1;
+        
+        System.out.println("Количество повторяющихся значений: " + countMap); // выводим в терминал для проверки;
 
         List<Map.Entry<String, Integer>> sortedList = countMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .toList();
+                .stream() // stream() создает поток данных для коллекции или массива, что позволяет последовательно обрабатывать элементы этой коллекции или массива с помощью различных операций;
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()) // Метод comparingByValue() создает компаратор, который сравнивает значения элементов потока. Этот метод может быть использован в методе sorted() для сортировки элементов потока по значениям. Метод reversed() создает компаратор, который обращает порядок сортировки элементов. 
+                .toList(); // Метод toList() преобразует элементы потока в список;
 
         for (Map.Entry<String, Integer> entry : sortedList) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
